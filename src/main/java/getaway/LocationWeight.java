492f9163222a8076dcf32f2fa12985d11c2e5545
laemmel <gregor.laemmel@gmail.com>
2016-03-03 16:39:37 +0100

    initial commit

---
+++ src/main/java/getaway/LocationWeight.java
@@ -0,0 +1,20 @@
+/****************************************************************************/
+// YourGetawaySpot, see https://github.com/GrgrLmml/getaway
+// Copyright (C) 2016 Gregor Laemmel (gregor.laemmel@gmail.com)
+/****************************************************************************/
+//
+//   This file is part of YourGetawaySpot.
+//   YourGetawaySpot is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 2 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+
+package getaway;
+
+/**
+ * Created by laemmel on 03.03.16.
+ */
+public class LocationWeight {
+}
